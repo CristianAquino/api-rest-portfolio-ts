7d6e4b0105fd021f9b47d9f71d209a45a17f6ba2
cristianaquino <ycristian10@gmail.com>
2024-01-24 00:53:04 -0500

    feat: barrel

---
+++ src/swagger-schemas/index.ts
@@ -0,0 +1,5 @@
+export * from "./image_swagger.schema";
+export * from "./project_swagger.schema";
+export * from "./skill_swagger.schema";
+export * from "./social_swagger.schema";
+export * from "./user_swagger.schema";
