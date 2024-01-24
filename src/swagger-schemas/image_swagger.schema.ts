5a15475303e84defb9c59ebbf54f3247567a1ed6
cristianaquino <ycristian10@gmail.com>
2024-01-24 00:52:34 -0500

    feat: update swagger schema

--- src/swagger-schemas/image_swagger.schema.ts
+++ src/swagger-schemas/image_swagger.schema.ts
@@ -1,22 +1,25 @@
+const permanData = {
+  thumbnail: {
+    type: "string",
+    description: "Image thumbnail",
+    example: "https://example.com/thumbnail.jpg",
+  },
+  small: {
+    type: "string",
+    description: "Image small",
+    example: "https://example.com/small.jpg",
+  },
+};
 export const allDataImageSwaggerSchema = {
   type: "array",
   items: {
     type: "object",
     properties: {
+      ...permanData,
       id: {
         type: "string",
         description: "User ID",
         example: "2e63341a-e627-48ac-bb1a-9d56e2e9cc4f",
-      },
-      thumbnail: {
-        type: "string",
-        description: "Image thumbnail",
-        example: "https://example.com/thumbnail.jpg",
-      },
-      small: {
-        type: "string",
-        description: "Image small",
-        example: "https://example.com/small.jpg",
       },
     },
   },
@@ -24,31 +27,13 @@ export const allDataImageSwaggerSchema = {
 export const dataImageSwaggerSchema = {
   type: "object",
   properties: {
-    thumbnail: {
-      type: "string",
-      description: "Image thumbnail",
-      example: "https://example.com/thumbnail.jpg",
-    },
-    small: {
-      type: "string",
-      description: "Image small",
-      example: "https://example.com/small.jpg",
-    },
+    ...permanData,
   },
 };
 export const insertDataImageSwaggerSchema = {
   type: "object",
   properties: {
-    thumbnail: {
-      type: "string",
-      description: "Image thumbnail",
-      example: "https://example.com/thumbnail.jpg",
-    },
-    small: {
-      type: "string",
-      description: "Image small",
-      example: "https://example.com/small.jpg",
-    },
+    ...permanData,
     user: {
       $ref: "#/components/schemas/DataUser",
     },
