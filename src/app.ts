b85088d904efce41c47fb6825fb3d940f3a6d33a
cristianaquino <ycristian10@gmail.com>
2024-01-24 00:54:35 -0500

    feat: base

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,23 @@
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerSetup from "./docs/swagger";
 // import { handleError, logs } from "./middlewares";
-// import {
+import { userRoute } from "./routes";
 //   authRoute,
-//   orderRoute,
-//   productRoute,
-//   stripeRoute,
-//   templateRoute,
-//   userRoute,
-// } from "./routes";
 const app = express();
 
 // middlewars
 app.use(cors());
-// app.use((req, res, next) => {
-//   if (req.originalUrl.includes("/api/v1/stripe/webhook")) {
-//     return next();
-//   }
-//   return bodyParser.json()(req, res, next);
-// });
+app.use(express.json());
 // app.use(cookieParser());
 // app.use(logs);
 
 // routes
 // app.use("/api/v1/auth", authRoute);
-// app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/product", productRoute);
-// app.use("/api/v1/order", orderRoute);
-// app.use("/api/v1/stripe", stripeRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
 // errors
 // app.use(handleError);
