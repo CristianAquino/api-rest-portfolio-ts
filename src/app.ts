6f2af6eff7ef09ec38cda7576d8de8a084a06943
cristianaquino <ycristian10@gmail.com>
2024-01-25 18:58:40 -0500

    feat: add route for auth

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerSetup from "./docs/swagger";
-import { userRoute } from "./routes";
+import { authRoute, userRoute } from "./routes";
 import { handleError } from "./middlewares";
 const app = express();
 
@@ -14,10 +14,10 @@ app.use(express.json());
 // app.use(logs);
 
 // routes
-// app.use("/api/v1/auth", authRoute);
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
+app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
