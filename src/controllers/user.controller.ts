0ebb74d52487a570c0f4863461b2a24ca2d426be
cristianaquino <ycristian10@gmail.com>
2024-01-24 22:27:33 -0500

    feat: user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { InsertDataUser, UpdateDataUser, UserInfo } from "../services";
+import { CreatedError } from "../utils";
 
 async function getOneUser(req: Request, res: Response) {
   try {
@@ -8,18 +9,21 @@ async function getOneUser(req: Request, res: Response) {
   } catch (error) {}
 }
 
-async function postCreateUser(req: Request, res: Response) {
+async function postCreateUser(req: Request, res: Response, next: NextFunction) {
   try {
-    const { body } = req;
-    const response = await InsertDataUser(body);
-    return res.status(200).json(response);
-  } catch (error) {}
+    const response = await InsertDataUser(req.body);
+    return res.status(200).json({ message: response });
+  } catch (error) {
+    if (error instanceof Error) {
+      next(error);
+    }
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 async function putUpdateDataUser(req: Request, res: Response) {
   try {
-    const { body } = req;
-    const response = await UpdateDataUser(body);
+    const response = await UpdateDataUser(req.body);
     return res.status(200).json(response);
   } catch (error) {}
 }
