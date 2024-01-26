5ffca061a36f0d5fec7e5016fd5db28c7f80e075
cristianaquino <ycristian10@gmail.com>
2024-01-25 21:20:24 -0500

    feat: add new error type

--- src/utils/errors_type.utils.ts
+++ src/utils/errors_type.utils.ts
@@ -2,7 +2,16 @@ import { errorType } from "../types";
 
 function validationError(type: string) {
   const dataError: errorType = {
+    NotFoundError: {
+      code: 404,
+    },
+    InvalidTokenError: {
+      code: 498,
+    },
     CreatedError: {
+      code: 500,
+    },
+    UpdatedError: {
       code: 500,
     },
     ConextionDBError: {
