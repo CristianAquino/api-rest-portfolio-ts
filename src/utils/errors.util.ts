9581f22443bd77f9209e9faa5c7eacaa013eaff3
cristianaquino <ycristian10@gmail.com>
2024-01-25 21:20:39 -0500

    feat: add new class of errors

--- src/utils/errors.util.ts
+++ src/utils/errors.util.ts
@@ -11,3 +11,24 @@ export class ConextionDBError extends Error {
     this.name = "ConextionDBError";
   }
 }
+
+export class UpdatedError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "UpdatedError";
+  }
+}
+
+export class NotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "NotFoundError";
+  }
+}
+
+export class InvalidTokenError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "InvalidTokenError";
+  }
+}
