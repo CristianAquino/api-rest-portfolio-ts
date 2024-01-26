79e2a4dc257e880f02f66256c0b27a56f41b481a
cristianaquino <ycristian10@gmail.com>
2024-01-25 21:34:02 -0500

    feat: update service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Users } from "../entities";
 import { UserDTO } from "../entities/DTO";
-import { UserRegisterType } from "../types";
-import { CreatedError } from "../utils";
+import { UpdateUserDataType } from "../types";
+import { NotFoundError, UpdatedError } from "../utils";
 
 async function UserInfo() {
   const user = await Users.find();
@@ -9,21 +9,14 @@ async function UserInfo() {
   return userDTO;
 }
 
-async function InsertDataUser(user: UserRegisterType) {
-  const cUser = new Users();
-  cUser.name = user.name;
-  cUser.first_name = user.first_name;
-  cUser.second_name = user.second_name;
-  cUser.description = user.description;
-  cUser.email = user.email;
-  cUser.password = user.password;
-  cUser.cv_link = user.cv_link;
-  const newU = await cUser.save();
-  if (!newU) throw new CreatedError("Could not register in the database");
-  return "user created";
+async function UpdateDataUser(data: UpdateUserDataType) {
+  const { id, ...rest } = data;
+  const user = await Users.findOneBy({ id });
+  if (!user) throw new NotFoundError("User not found");
+  const userUpdate = await Users.update(id, rest);
+  if (userUpdate.affected === 0)
+    throw new UpdatedError("Could not update the user");
+  return "user updated";
 }
 
-async function UpdateDataUser(user: Users) {
-  // return await Users.update(params.id, params);
-}
-export { InsertDataUser, UpdateDataUser, UserInfo };
+export { UpdateDataUser, UserInfo };
