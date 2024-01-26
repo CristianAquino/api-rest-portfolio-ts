24402b6c5acc00f8301c9f4a18b43084020d7338
cristianaquino <ycristian10@gmail.com>
2024-01-25 21:21:47 -0500

    feat: update user types

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
-import { CreateUserSchema } from "../schemas";
+import { CreateUserSchema, UpdateUserSchema } from "../schemas";
 
 type UserRegisterType = z.infer<typeof CreateUserSchema>["body"];
+type UpdateUserDataType = z.infer<typeof UpdateUserSchema>["body"];
 
-export { UserRegisterType };
+export { UserRegisterType, UpdateUserDataType };
