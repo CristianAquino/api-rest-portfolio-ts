66767becd2708703ceb32b55d78398245c946097
cristianaquino <ycristian10@gmail.com>
2024-01-27 17:51:32 -0500

    feat: add id decode token to request

--- src/middlewares/authorization_validator.middleware.ts
+++ src/middlewares/authorization_validator.middleware.ts
@@ -1,13 +1,19 @@
-import { NextFunction, Request, Response, request } from "express";
-import { InvalidTokenError, confirmToken } from "../utils";
+import { NextFunction, Response } from "express";
 import { JsonWebTokenError } from "jsonwebtoken";
+import { RequestExtens } from "../types";
+import { InvalidTokenError, confirmToken } from "../utils";
 
-async function verifyToken(req: Request, res: Response, next: NextFunction) {
+async function verifyToken(
+  req: RequestExtens<string>,
+  res: Response,
+  next: NextFunction
+) {
   const { authorization } = req.headers;
   try {
     const token = authorization?.split(" ")[1] ?? " ";
     const decode: any = confirmToken(token);
     if (!decode) throw new InvalidTokenError("The token entered is not valid");
+    req.id = decode.id;
   } catch (error) {
     if (error instanceof JsonWebTokenError) {
       return next(error);
