85bc18c2a9ed36dafbd3a19f5307df122b1cea58
cristianaquino <ycristian10@gmail.com>
2024-01-28 10:44:21 -0500

    feat: base

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,8 @@ import cors from "cors";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerSetup from "./docs/swagger";
+import { imageRoute } from "./routes";
 import { handleError } from "./middlewares";
-import { userRoute } from "./routes";
 const app = express();
 
 // middlewars
@@ -14,11 +14,10 @@ app.use(express.json());
 // app.use(logs);
 
 // routes
-// app.use("/api/v1/auth", authRoute);
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
-app.use("/api/v1/user", userRoute);
+app.use("/api/v1/image", imageRoute);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
 // errors
