5d0c6eca6897bb2425ef02d56861573e41157dcc
cristianaquino <ycristian10@gmail.com>
2024-01-28 01:09:47 -0500

    feat: update all route auth

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,7 +1,18 @@
 import { Router } from "express";
-import { postCreateUser, postLoginUser } from "../controllers";
-import { schemaValidator } from "../middlewares";
-import { CreateUserSchema, UserLoginSchema } from "../schemas";
+import {
+  getLogoutUser,
+  postCodeChangePassword,
+  postCreateUser,
+  postLoginUser,
+  putChangePassword,
+} from "../controllers";
+import { schemaValidator, verifyToken } from "../middlewares";
+import {
+  ChangeUserPasswordSchema,
+  CreateUserSchema,
+  HeaderValidateSchema,
+  UserLoginSchema,
+} from "../schemas";
 
 const authRoute = Router();
 
@@ -25,20 +36,27 @@ const authRoute = Router();
  *      application/json:
  *       schema:
  *        $ref: '#/components/schemas/ResponseLogin'
- *    400:
- *     description: invalid input
- *     content:
- *      application/json:
- *       schema:
- *        $ref: '#/components/schemas/ErrorResponse'
  *    404:
  *     description: email or password incorrect
  *     content:
  *      application/json:
  *       schema:
- *        type: object
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    422:
+ *     description: validation exception
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ErrorResponse'
+ *    500:
+ *     description: error to created token
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
  */
 authRoute.post("/signin", schemaValidator(UserLoginSchema), postLoginUser);
+
 /**
  *@swagger
  * /auth/signup:
@@ -59,17 +77,141 @@ authRoute.post("/signin", schemaValidator(UserLoginSchema), postLoginUser);
  *      application/json:
  *       schema:
  *        $ref: '#/components/schemas/MessageResponse'
- *    400:
- *     description: invalid input
+ *    402:
+ *     description: user already exists
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    422:
+ *     description: validation exception
  *     content:
  *      application/json:
  *       schema:
  *        $ref: '#/components/schemas/ErrorResponse'
- *    402:
- *     description: user already exists
- *    422:
- *     description: validation exception
+ *    500:
+ *     description: error to created new user
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
  */
 authRoute.post("/signup", schemaValidator(CreateUserSchema), postCreateUser);
-authRoute.post("/logout", (req, res) => {});
+
+/**
+ *@swagger
+ * /auth/logout:
+ *  get:
+ *   tags:
+ *    - Auth
+ *   summary: logout user
+ *   description: logout user
+ *   responses:
+ *    200:
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    404:
+ *     description: user not found
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    422:
+ *     description: validation exception
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ErrorResponse'
+ *   security:
+ *    - bearerAuth: []
+ */
+authRoute.get(
+  "/logout",
+  [schemaValidator(HeaderValidateSchema), verifyToken],
+  getLogoutUser
+);
+
+/**
+ *@swagger
+ * /auth/code-change-password:
+ *  post:
+ *   tags:
+ *    - Auth
+ *   summary: generate code for change password user
+ *   description: generate code for change password user
+ *   responses:
+ *    201:
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    403:
+ *     description: resend error to send email
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    404:
+ *     description: user not found
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    422:
+ *     description: validation exception
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ErrorResponse'
+ *   security:
+ *    - bearerAuth: []
+ */
+authRoute.post(
+  "/code-change-password",
+  [schemaValidator(HeaderValidateSchema), verifyToken],
+  postCodeChangePassword
+);
+
+/**
+ *@swagger
+ * /auth/change-password:
+ *  put:
+ *   tags:
+ *    - Auth
+ *   summary: change password user
+ *   description: change password user
+ *   requestBody:
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/ChangePassword'
+ *   responses:
+ *    201:
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    404:
+ *     description: user not found
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/MessageResponse'
+ *    422:
+ *     description: validation exception
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/ErrorResponse'
+ *   security:
+ *    - bearerAuth: []
+ */
+authRoute.put(
+  "/change-password",
+  [schemaValidator(ChangeUserPasswordSchema), verifyToken],
+  putChangePassword
+);
+
 export { authRoute };
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,22 @@
+import { randomUUID } from "crypto";
 import { Users } from "../entities";
-import { LoginType, UserRegisterType } from "../types";
+import {
+  ChangePasswordType,
+  LoginType,
+  ParamsType,
+  UserRegisterType,
+} from "../types";
 import {
   CreatedError,
   NotFoundError,
+  ResendEmailError,
   comparePassword,
   createToken,
   encryptPassword,
+  sendCodeWithEmail,
 } from "../utils";
-import { randomUUID } from "crypto";
 
-async function loginUser(data: LoginType) {
+async function loginUser({ data }: Pick<ParamsType<LoginType>, "data">) {
   const { email, password } = data;
   const user = (await Users.findOneBy({ email })) as Users;
   const matchPassword =
@@ -27,19 +34,67 @@ async function loginUser(data: LoginType) {
   return token;
 }
 
-async function insertDataUser(user: UserRegisterType) {
+async function insertDataUser({
+  data,
+}: Pick<ParamsType<UserRegisterType>, "data">) {
   const cUser = new Users();
-  const hashPassword = await encryptPassword(user.password);
-  cUser.name = user.name;
-  cUser.first_name = user.first_name;
-  cUser.second_name = user.second_name;
-  cUser.description = user.description;
-  cUser.email = user.email;
+  const hashPassword = await encryptPassword(data.password);
+  cUser.name = data.name;
+  cUser.first_name = data.first_name;
+  cUser.second_name = data.second_name;
+  cUser.description = data.description;
+  cUser.email = data.email;
   cUser.password = hashPassword;
-  cUser.cv_link = user.cv_link;
+  cUser.cv_link = data.cv_link;
 
   const newU = await cUser.save();
   if (!newU) throw new CreatedError("Could not register in the database");
   return "user created";
 }
-export { loginUser, insertDataUser };
+
+async function removeLoginUser({ uuid }: Omit<ParamsType<unknown>, "data">) {
+  const user = await Users.findOneBy({ uuid });
+  if (!user) throw new NotFoundError("User not found");
+  user.uuid = null;
+  await user.save();
+  return "user logout";
+}
+
+async function generateCodeChangePassword({
+  uuid,
+}: Omit<ParamsType<unknown>, "data">) {
+  const user = await Users.findOneBy({ uuid });
+  if (!user) throw new NotFoundError("User not found");
+  user.code = "" + Math.floor(Math.random() * (9999 - 1000) + 1000);
+  const { error } = await sendCodeWithEmail({ data: user });
+  if (error !== null) {
+    throw new ResendEmailError(error?.message);
+  }
+  user.save();
+  return "Check your email and enter the generated code";
+}
+
+async function changePassword({ uuid, data }: ParamsType<ChangePasswordType>) {
+  const { oldpassword, newpassword, code } = data;
+  const user = await Users.findOneBy({ uuid });
+  if (!user) throw new NotFoundError("User not found");
+  const matchPassword =
+    user === null ? false : await comparePassword(oldpassword, user.password);
+  if (!matchPassword)
+    throw new NotFoundError(
+      "Your old password is not registered in our system, please try again"
+    );
+  if (user.code !== code) throw new NotFoundError("Entered code is incorrect");
+  const hashPassword = await encryptPassword(newpassword);
+  user.password = hashPassword;
+  user.code = null;
+  user.save();
+  return "Password changed";
+}
+export {
+  generateCodeChangePassword,
+  insertDataUser,
+  loginUser,
+  removeLoginUser,
+  changePassword,
+};
