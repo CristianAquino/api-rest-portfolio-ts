d6f160eb1b3416cd45fd8afcf34cd52ec5a1115b
cristianaquino <ycristian10@gmail.com>
2024-01-30 00:36:06 -0500

    feat: update types for social and auth

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import {
   BaseHeaderSchema,
   ChangeUserPasswordSchema,
+  IdentifierIdSchema,
   UserLoginSchema,
 } from "../schemas";
 
@@ -13,5 +14,12 @@ type ParamsType<T> = {
 type LoginType = z.infer<typeof UserLoginSchema>["body"];
 type HeaderAuthorizationTye = z.infer<typeof BaseHeaderSchema>;
 type ChangePasswordType = z.infer<typeof ChangeUserPasswordSchema>["body"];
+type IdType = z.infer<typeof IdentifierIdSchema>["params"];
 
-export { ChangePasswordType, HeaderAuthorizationTye, LoginType, ParamsType };
+export {
+  ChangePasswordType,
+  HeaderAuthorizationTye,
+  LoginType,
+  ParamsType,
+  IdType,
+};
