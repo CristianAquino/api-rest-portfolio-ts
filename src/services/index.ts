9ebceb686e2c0c6507908b6719450a44f30d76f8
cristianaquino <ycristian10@gmail.com>
2024-01-31 10:15:00 -0500

    feat: barrel

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 export * from "./auth.service";
 export * from "./image.service";
+export * from "./project.service";
 export * from "./skill.service";
 export * from "./social.service";
 export * from "./user.service";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 export * from "./auth.type";
 export * from "./error.type";
 export * from "./image.type";
+export * from "./project.type";
 export * from "./request.type";
 export * from "./skill.type";
 export * from "./skill_enum.type";
