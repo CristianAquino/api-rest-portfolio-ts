2d1da859061c16628c835535eac8cd386ed80355
cristianaquino <ycristian10@gmail.com>
2024-02-01 12:09:40 -0500

    feat: updating naming of consts

--- src/swagger-schemas/image_swagger.schema.ts
+++ src/swagger-schemas/image_swagger.schema.ts
@@ -1,47 +1,43 @@
 const permanData = {
+  id: {
+    type: "string",
+    description: "User ID",
+    example: "2e63341a-e627-48ac-bb1a-9d56e2e9cc4f",
+  },
   thumbnail: {
     type: "string",
     description: "Image thumbnail",
     example: "https://example.com/thumbnail.jpg",
   },
-  small: {
-    type: "string",
-    description: "Image small",
-    example: "https://example.com/small.jpg",
-  },
 };
-export const allDataImageSwaggerSchema = {
+export const AllDataImageSwaggerSchema = {
   type: "array",
   items: {
     type: "object",
     properties: {
-      id: {
-        type: "string",
-        description: "User ID",
-        example: "2e63341a-e627-48ac-bb1a-9d56e2e9cc4f",
-      },
       ...permanData,
+      small: {
+        type: "string",
+        description: "Image small",
+        example: "https://example.com/small.jpg",
+      },
     },
   },
 };
-export const dataImageSwaggerSchema = {
+export const OneDataImageSwaggerSchema = {
   type: "object",
   properties: {
     ...permanData,
+    small: {
+      type: "string",
+      description: "Image small",
+      example: "https://example.com/small.jpg",
+    },
   },
 };
-export const insertDataImageSwaggerSchema = {
+export const SendDataImageSwaggerSchema = {
   type: "object",
   properties: {
-    id: {
-      type: "string",
-      description: "User ID",
-      example: "2e63341a-e627-48ac-bb1a-9d56e2e9cc4f",
-    },
-    thumbnail: {
-      type: "string",
-      description: "Image thumbnail",
-      example: "https://example.com/thumbnail.jpg",
-    },
+    ...permanData,
   },
 };
