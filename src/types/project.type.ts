fa5a1ef88b3ed5c989ce0f06aa1cb665c3db5ab6
cristianaquino <ycristian10@gmail.com>
2024-02-03 22:35:57 -0500

    feat: update route and types of project

--- src/types/project.type.ts
+++ src/types/project.type.ts
@@ -1,7 +1,21 @@
 import { z } from "zod";
-import { CreateProjectSchema, UpdateProjectSchema } from "../schemas";
+import {
+  CreateProjectSchema,
+  UpdateImageProjectSchema,
+  UpdateProjectSchema,
+  UpdateSkillsProjectSchema,
+} from "../schemas";
 
 type ProjectCreateType = z.infer<typeof CreateProjectSchema>["body"];
 type ProjectUpdateType = z.infer<typeof UpdateProjectSchema>["body"];
+type ProjectUpdateImageType = z.infer<typeof UpdateImageProjectSchema>["body"];
+type ProjectUpdateSkillsType = z.infer<
+  typeof UpdateSkillsProjectSchema
+>["body"];
 
-export { ProjectCreateType, ProjectUpdateType };
+export {
+  ProjectCreateType,
+  ProjectUpdateType,
+  ProjectUpdateImageType,
+  ProjectUpdateSkillsType,
+};
