2d0f77c21db68ce13dd4dd61bc660cd31dc18b4d
cristianaquino <ycristian10@gmail.com>
2024-02-06 16:04:05 -0500

    feat: add for parse error of typeorm

--- src/middlewares/handle_error.middleware.ts
+++ src/middlewares/handle_error.middleware.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { TypeORMError } from "typeorm";
 import { validationError } from "../utils";
 
 export function handleError(
@@ -7,9 +8,15 @@ export function handleError(
   res: Response,
   next: NextFunction
 ) {
+  if (error instanceof TypeORMError) {
+    return res.status(400).json({
+      message: error.message.substring(error.message.indexOf(":") + 1).trim(),
+    });
+  }
   if (error instanceof Error) {
     const { code } = validationError(error.name);
     return res.status(code).send({ message: error.message });
   }
+
   return res.status(500).json({ message: "Internal Server Error" });
 }
