6cf8545377320969bc2b4d9eeafae9c4229de439
cristianaquino <ycristian10@gmail.com>
2024-02-06 02:01:58 -0500

    feat: update dto for user

--- src/entities/DTO/AllUserDTO.ts
+++ src/entities/DTO/AllUserDTO.ts
@@ -9,8 +9,6 @@ export class AllUserDTO {
   description: string;
   email: string;
   cv_link: string | null;
-  uuid: string | null;
-  code: string | null;
   image: ImagesDTO;
   socials: SocialDTO[];
   skills: SkillDTO[];
@@ -23,8 +21,6 @@ export class AllUserDTO {
     this.description = user.description;
     this.email = user.email;
     this.cv_link = user.cv_link;
-    this.uuid = user.uuid;
-    this.code = user.code;
     this.image = new ImagesDTO(user.image);
     this.socials = user.socials.map((s) => new SocialDTO(s)) ?? [];
     this.skills = user.skills.map((s) => new SkillDTO(s)) ?? [];
